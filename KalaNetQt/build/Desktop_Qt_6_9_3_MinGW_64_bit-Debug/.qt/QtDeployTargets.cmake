e9b5478d7afb3b2c81454d5f2751e7cfe2d89ff1
Raya Asgari <asgariraya@gmail.com>
2026-02-09 22:08:45 +0330

    Remove build files from Git

--- KalaNetQt/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_KalaNetQt_FILE D:/Qt/KalaNet/KalaNetQt/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/KalaNetQt.exe)
-set(__QT_DEPLOY_TARGET_KalaNetQt_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_KalaNetQt_RUNTIME_DLLS C:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
